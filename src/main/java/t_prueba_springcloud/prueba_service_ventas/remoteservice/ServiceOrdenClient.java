37d901c293d4c3eb766833cd81e0e2335c47b041
zchanway <146023170+zchanway@users.noreply.github.com>
2024-10-11 23:20:04 -0500

    serivio venta

--- src/main/java/t_prueba_springcloud/prueba_service_ventas/remoteservice/ServiceOrdenClient.java
+++ src/main/java/t_prueba_springcloud/prueba_service_ventas/remoteservice/ServiceOrdenClient.java
@@ -3,9 +3,11 @@ package t_prueba_springcloud.prueba_service_ventas.remoteservice;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@FeignClient(name ="prueba-service-mantenimiento")
 
 public interface ServiceOrdenClient {
 
 
-
+    @GetMapping("/orden")
+    String consultarOrden();
 }
